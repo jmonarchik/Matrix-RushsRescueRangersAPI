523bf55c6ba88b89284cd4bda89454b8c5e1832a
Rush Myers <myersrush1986@gmail.com>
2018-05-17 14:30:54 -0400

    Change connection string value in Design time context factory

---
+++ RescueRangers.API/DesignTimeDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using RescueRangers.API.Entities;
+
+namespace Web
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<WebApiDbContext>
+    {
+        public WebApiDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<WebApiDbContext>();
+
+            var connectionString = configuration["ConnectionStrings:DefaultConnection"];
+
+            builder.UseSqlite(connectionString);
+
+            return new WebApiDbContext(builder.Options);
+        }
+    }
+}
