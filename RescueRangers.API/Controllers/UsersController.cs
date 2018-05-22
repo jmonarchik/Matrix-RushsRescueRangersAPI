67d07ea87182af3e355641c157b1f5ac9cca3994
Rush Myers <myersrush1986@gmail.com>
2018-05-22 18:12:46 -0400

    Create Controller and Dto model for User

---
+++ RescueRangers.API/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RescueRangers.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RescueRangers.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        [HttpPost()]
+        public IActionResult CreateUser([FromBody] UserDto user)
+        {
+            return Ok(user);
+        }
+    }
+}
---
+++ RescueRangers.API/Models/UserDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RescueRangers.API.Models
+{
+    public class UserDto
+    {
+        public int userId { get; set; }
+        public string name { get; set; }
+        public string userName { get; set; }
+        public string phoneNo { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
